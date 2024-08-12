5817b079f0fd335974030aed7f2e04fcfcbbd12a
hwmin414 <hwmin414@kiast.ac.kr>
2024-08-12 21:04:00 +0900

    feat: merge from main to deploy

--- client-ts/src/Contracts.ts
+++ client-ts/src/Contracts.ts
@@ -1,5 +1,5 @@
-export const QnAAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
-// export const QnAAddress = '0x8B5820801e751920bCC3A9B862bba53154B200D1';
+// export const QnAAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+export const QnAAddress = '0x8B5820801e751920bCC3A9B862bba53154B200D1';
 export const QnAAbi = [
   {
     inputs: [
@@ -448,8 +448,8 @@ export const QnAAbi = [
     type: 'function',
   },
 ];
-export const QaCoinAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-// export const QaCoinAddress = '0xF892Ef13f996587D8F59C3a8C4f9beA049d1C5BD';
+// export const QaCoinAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const QaCoinAddress = '0xF892Ef13f996587D8F59C3a8C4f9beA049d1C5BD';
 export const QaCoinAbi = [
   {
     inputs: [],
